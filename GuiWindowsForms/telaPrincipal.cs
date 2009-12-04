bf3a52162b8de784d5b96995bdb3c38a277048d7
alexandregsalves@hotmail.com <alexandregsalves@hotmail.com@1f52cf30-ca48-11de-a9c8-2fbcfba63733>
2009-12-04 04:51:41 +0000

    Concluido o estilo das telas telaLogin, telaPrincipal, telaPrincipalAluno.

--- GuiWindowsForms/telaPrincipal.cs
+++ GuiWindowsForms/telaPrincipal.cs
@@ -11,21 +11,64 @@ namespace GuiWindowsForms
 {
     public partial class telaPrincipal : Form
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(telaPrincipal));
         public telaPrincipal()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Botão chama a tela principal de Aluno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnAluno_Click(object sender, EventArgs e)
         {
             telaAlunoPrincipal tela = new telaAlunoPrincipal();
             tela.Show();
         }
 
-        private void btnAluno_MouseHover(object sender, EventArgs e)
+        /// <summary>
+        /// Quando o mouse entra sobre a área da figura, sua imagem é substituida e a label é exibida
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAluno_MouseEnter(object sender, EventArgs e)
         {
-            this.btnAluno.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_aluno_142x115_over;
+            this.btnAluno.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_aluno_142x113_hover;
+            lblAlunoOculto.Visible = true;
+        }
+
+        /// <summary>
+        /// Quando o mouse sai da área da figura, sua imagem é substituida pela original e a label é ocultada
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAluno_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnAluno.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_aluno_142x113;
+            lblAlunoOculto.Visible = false;
+        }
+
+        /// <summary>
+        /// Quando o mouse entra sobre a área da figura, sua imagem é substituida e a label é exibida
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFuncionario_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnFuncionario.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_prof_142x113;
+            lblFuncionarioOculto.Visible = false;
+        }
+
+        /// <summary>
+        /// Quando o mouse sai da área da figura, sua imagem é substituida pela original e a label é ocultada
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFuncionario_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnFuncionario.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_prof_142x113_hover;
+            lblFuncionarioOculto.Visible = true;
         }
 
     }
