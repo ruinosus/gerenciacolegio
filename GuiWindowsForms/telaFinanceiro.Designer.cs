4d8882e32ebb0b1069cb430b4b29c497f63e8dd8
pimentel.sales@hotmail.com <pimentel.sales@hotmail.com@1f52cf30-ca48-11de-a9c8-2fbcfba63733>
2009-12-14 16:44:17 +0000

    Iniciado a tela de Financeiro obs: o filtro será dinamico, ainda esta no esboço      Importante identificar os tipos de consultas que serão      realizadas.

---
+++ GuiWindowsForms/telaFinanceiro.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GuiWindowsForms
+{
+    partial class telaFinanceiro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.lblHelloUsuario = new System.Windows.Forms.Label();
+            this.pnlFiltro = new System.Windows.Forms.Panel();
+            this.lblEntrada = new System.Windows.Forms.Label();
+            this.dtgResultado = new System.Windows.Forms.DataGridView();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtBusca = new System.Windows.Forms.TextBox();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.ckbPendente = new System.Windows.Forms.CheckBox();
+            this.ckbTodos = new System.Windows.Forms.CheckBox();
+            this.ckbAtivo = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlFiltro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgResultado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.BackColor = System.Drawing.Color.Transparent;
+            this.btnDesconectar.BackgroundImage = global::GuiWindowsForms.Properties.Resources.icone_desconectar;
+            this.btnDesconectar.FlatAppearance.BorderSize = 0;
+            this.btnDesconectar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDesconectar.Location = new System.Drawing.Point(950, 9);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(30, 30);
+            this.btnDesconectar.TabIndex = 9;
+            this.btnDesconectar.UseVisualStyleBackColor = false;
+            // 
+            // lblHelloUsuario
+            // 
+            this.lblHelloUsuario.AutoSize = true;
+            this.lblHelloUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblHelloUsuario.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold);
+            this.lblHelloUsuario.ForeColor = System.Drawing.Color.Yellow;
+            this.lblHelloUsuario.Location = new System.Drawing.Point(802, 9);
+            this.lblHelloUsuario.Name = "lblHelloUsuario";
+            this.lblHelloUsuario.Size = new System.Drawing.Size(133, 23);
+            this.lblHelloUsuario.TabIndex = 8;
+            this.lblHelloUsuario.Text = "Hello Usuário!";
+            // 
+            // pnlFiltro
+            // 
+            this.pnlFiltro.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlFiltro.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlFiltro.Controls.Add(this.label1);
+            this.pnlFiltro.Controls.Add(this.ckbAtivo);
+            this.pnlFiltro.Controls.Add(this.ckbTodos);
+            this.pnlFiltro.Controls.Add(this.ckbPendente);
+            this.pnlFiltro.Controls.Add(this.trackBar1);
+            this.pnlFiltro.Controls.Add(this.lblEntrada);
+            this.pnlFiltro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pnlFiltro.Location = new System.Drawing.Point(12, 145);
+            this.pnlFiltro.Name = "pnlFiltro";
+            this.pnlFiltro.Size = new System.Drawing.Size(203, 474);
+            this.pnlFiltro.TabIndex = 10;
+            // 
+            // lblEntrada
+            // 
+            this.lblEntrada.AutoSize = true;
+            this.lblEntrada.BackColor = System.Drawing.Color.White;
+            this.lblEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntrada.Location = new System.Drawing.Point(-2, 237);
+            this.lblEntrada.Name = "lblEntrada";
+            this.lblEntrada.Size = new System.Drawing.Size(200, 20);
+            this.lblEntrada.TabIndex = 0;
+            this.lblEntrada.Text = "SITUAÇÃO DOS ALUNOS";
+            // 
+            // dtgResultado
+            // 
+            this.dtgResultado.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dtgResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgResultado.Location = new System.Drawing.Point(249, 250);
+            this.dtgResultado.Name = "dtgResultado";
+            this.dtgResultado.Size = new System.Drawing.Size(701, 369);
+            this.dtgResultado.TabIndex = 11;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(277, 12);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(104, 30);
+            this.btnImprimir.TabIndex = 12;
+            this.btnImprimir.Text = "IMPRIMIR";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.AutoEllipsis = true;
+            this.btnPesquisar.BackColor = System.Drawing.Color.Transparent;
+            this.btnPesquisar.BackgroundImage = global::GuiWindowsForms.Properties.Resources.Search_76x76;
+            this.btnPesquisar.FlatAppearance.BorderSize = 0;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.Location = new System.Drawing.Point(832, 148);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(74, 75);
+            this.btnPesquisar.TabIndex = 14;
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            // 
+            // txtBusca
+            // 
+            this.txtBusca.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBusca.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.txtBusca.Location = new System.Drawing.Point(380, 171);
+            this.txtBusca.MaxLength = 60;
+            this.txtBusca.Name = "txtBusca";
+            this.txtBusca.Size = new System.Drawing.Size(415, 26);
+            this.txtBusca.TabIndex = 13;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.BackColor = System.Drawing.Color.Transparent;
+            this.btnVoltar.BackgroundImage = global::GuiWindowsForms.Properties.Resources.voltar_final_32x32;
+            this.btnVoltar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVoltar.FlatAppearance.BorderSize = 0;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Location = new System.Drawing.Point(39, 681);
+            this.btnVoltar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(33, 34);
+            this.btnVoltar.TabIndex = 16;
+            this.btnVoltar.Tag = "Voltar para tela anterior";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(3, 166);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(177, 45);
+            this.trackBar1.TabIndex = 17;
+            // 
+            // ckbPendente
+            // 
+            this.ckbPendente.AutoSize = true;
+            this.ckbPendente.Location = new System.Drawing.Point(25, 293);
+            this.ckbPendente.Name = "ckbPendente";
+            this.ckbPendente.Size = new System.Drawing.Size(72, 17);
+            this.ckbPendente.TabIndex = 18;
+            this.ckbPendente.Text = "Pendente";
+            this.ckbPendente.UseVisualStyleBackColor = true;
+            // 
+            // ckbTodos
+            // 
+            this.ckbTodos.AutoSize = true;
+            this.ckbTodos.Location = new System.Drawing.Point(25, 316);
+            this.ckbTodos.Name = "ckbTodos";
+            this.ckbTodos.Size = new System.Drawing.Size(56, 17);
+            this.ckbTodos.TabIndex = 19;
+            this.ckbTodos.Text = "Todos";
+            this.ckbTodos.UseVisualStyleBackColor = true;
+            // 
+            // ckbAtivo
+            // 
+            this.ckbAtivo.AutoSize = true;
+            this.ckbAtivo.Location = new System.Drawing.Point(25, 270);
+            this.ckbAtivo.Name = "ckbAtivo";
+            this.ckbAtivo.Size = new System.Drawing.Size(63, 17);
+            this.ckbAtivo.TabIndex = 20;
+            this.ckbAtivo.Text = "Regular";
+            this.ckbAtivo.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "SITUAÇÃO DOS ALUNOS";
+            // 
+            // telaFinanceiro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::GuiWindowsForms.Properties.Resources.modelo_tela_iconemenor1;
+            this.ClientSize = new System.Drawing.Size(994, 716);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtBusca);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.dtgResultado);
+            this.Controls.Add(this.pnlFiltro);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.lblHelloUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "telaFinanceiro";
+            this.Opacity = 0.8;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Colégio Conhecer - Educação Infantil ao Ensino Médio";
+            this.pnlFiltro.ResumeLayout(false);
+            this.pnlFiltro.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgResultado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Label lblHelloUsuario;
+        private System.Windows.Forms.Panel pnlFiltro;
+        private System.Windows.Forms.Label lblEntrada;
+        private System.Windows.Forms.DataGridView dtgResultado;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtBusca;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.CheckBox ckbAtivo;
+        private System.Windows.Forms.CheckBox ckbTodos;
+        private System.Windows.Forms.CheckBox ckbPendente;
+        private System.Windows.Forms.Label label1;
+    }
+}
