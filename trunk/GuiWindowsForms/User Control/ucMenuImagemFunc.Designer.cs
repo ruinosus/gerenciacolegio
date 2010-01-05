5bd8f0458d08c32b4846a6274f7632e08f141235
pimentel.sales@hotmail.com <pimentel.sales@hotmail.com@1f52cf30-ca48-11de-a9c8-2fbcfba63733>
2010-01-05 04:56:29 +0000

    User control para tela Funcionario - menuLateralFuncionario

---
+++ trunk/GuiWindowsForms/User Control/ucMenuImagemFunc.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace GuiWindowsForms.User_Control
+{
+    partial class ucMenuImagemFunc
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAtivo = new System.Windows.Forms.Label();
+            this.lblFone = new System.Windows.Forms.Label();
+            this.lblFuncao = new System.Windows.Forms.Label();
+            this.lblNomeFuncionario = new System.Windows.Forms.Label();
+            this.pctImagem = new System.Windows.Forms.PictureBox();
+            this.btnAdicionarImagem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblAtivo
+            // 
+            this.lblAtivo.AutoSize = true;
+            this.lblAtivo.BackColor = System.Drawing.Color.Transparent;
+            this.lblAtivo.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold);
+            this.lblAtivo.ForeColor = System.Drawing.Color.Green;
+            this.lblAtivo.Location = new System.Drawing.Point(147, 97);
+            this.lblAtivo.Name = "lblAtivo";
+            this.lblAtivo.Size = new System.Drawing.Size(49, 19);
+            this.lblAtivo.TabIndex = 140;
+            this.lblAtivo.Text = "Ativo";
+            // 
+            // lblFone
+            // 
+            this.lblFone.AutoSize = true;
+            this.lblFone.BackColor = System.Drawing.Color.Transparent;
+            this.lblFone.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold);
+            this.lblFone.ForeColor = System.Drawing.Color.Yellow;
+            this.lblFone.Location = new System.Drawing.Point(147, 65);
+            this.lblFone.Name = "lblFone";
+            this.lblFone.Size = new System.Drawing.Size(50, 19);
+            this.lblFone.TabIndex = 139;
+            this.lblFone.Text = "Fone ";
+            // 
+            // lblFuncao
+            // 
+            this.lblFuncao.AutoSize = true;
+            this.lblFuncao.BackColor = System.Drawing.Color.Transparent;
+            this.lblFuncao.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold);
+            this.lblFuncao.ForeColor = System.Drawing.Color.Yellow;
+            this.lblFuncao.Location = new System.Drawing.Point(147, 36);
+            this.lblFuncao.Name = "lblFuncao";
+            this.lblFuncao.Size = new System.Drawing.Size(63, 19);
+            this.lblFuncao.TabIndex = 138;
+            this.lblFuncao.Text = "Função";
+            // 
+            // lblNomeFuncionario
+            // 
+            this.lblNomeFuncionario.AutoSize = true;
+            this.lblNomeFuncionario.BackColor = System.Drawing.Color.Transparent;
+            this.lblNomeFuncionario.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold);
+            this.lblNomeFuncionario.ForeColor = System.Drawing.Color.Yellow;
+            this.lblNomeFuncionario.Location = new System.Drawing.Point(147, 3);
+            this.lblNomeFuncionario.Name = "lblNomeFuncionario";
+            this.lblNomeFuncionario.Size = new System.Drawing.Size(169, 19);
+            this.lblNomeFuncionario.TabIndex = 137;
+            this.lblNomeFuncionario.Text = "Nome do Funcionário";
+            // 
+            // pctImagem
+            // 
+            this.pctImagem.BackColor = System.Drawing.Color.Transparent;
+            this.pctImagem.Location = new System.Drawing.Point(3, 3);
+            this.pctImagem.Name = "pctImagem";
+            this.pctImagem.Size = new System.Drawing.Size(127, 134);
+            this.pctImagem.TabIndex = 136;
+            this.pctImagem.TabStop = false;
+            // 
+            // btnAdicionarImagem
+            // 
+            this.btnAdicionarImagem.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdicionarImagem.BackgroundImage = global::GuiWindowsForms.Properties.Resources.adicionar_final_32x32;
+            this.btnAdicionarImagem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdicionarImagem.FlatAppearance.BorderSize = 0;
+            this.btnAdicionarImagem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdicionarImagem.Location = new System.Drawing.Point(43, 143);
+            this.btnAdicionarImagem.Margin = new System.Windows.Forms.Padding(0);
+            this.btnAdicionarImagem.Name = "btnAdicionarImagem";
+            this.btnAdicionarImagem.Size = new System.Drawing.Size(34, 34);
+            this.btnAdicionarImagem.TabIndex = 135;
+            this.btnAdicionarImagem.Tag = "Adicionar Foto";
+            this.btnAdicionarImagem.UseVisualStyleBackColor = false;
+            this.btnAdicionarImagem.Click += new System.EventHandler(this.btnAdicionarImagem_Click);
+            // 
+            // ucMenuImagemFunc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.lblAtivo);
+            this.Controls.Add(this.lblFone);
+            this.Controls.Add(this.lblFuncao);
+            this.Controls.Add(this.lblNomeFuncionario);
+            this.Controls.Add(this.pctImagem);
+            this.Controls.Add(this.btnAdicionarImagem);
+            this.Name = "ucMenuImagemFunc";
+            this.Size = new System.Drawing.Size(380, 178);
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAtivo;
+        private System.Windows.Forms.Label lblFone;
+        private System.Windows.Forms.Label lblFuncao;
+        private System.Windows.Forms.Label lblNomeFuncionario;
+        private System.Windows.Forms.PictureBox pctImagem;
+        private System.Windows.Forms.Button btnAdicionarImagem;
+    }
+}
---
+++ trunk/GuiWindowsForms/User Control/ucMenuLateralFunc.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GuiWindowsForms.User_Control
+{
+    public partial class ucMenuLateralFunc : UserControl
+    {
+        public ucMenuLateralFunc()
+        {
+            InitializeComponent();
+        }
+
+        #region BUTTON DADOS PESSOAIS
+
+        private void btnDadosPessoais_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Program.ultimaTela = 17;
+            telaFuncionario telaafuncdados = telaFuncionario.getInstancia();
+            telaafuncdados.Show();
+        }
+
+        private void btnDadosPessoais_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnDadosPessoais.BackgroundImage = global::GuiWindowsForms.Properties.Resources.func_dados_71x62_hover;
+            lblDados_Menu.Visible = true;
+        }
+
+        private void btnDadosPessoais_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnDadosPessoais.BackgroundImage = global::GuiWindowsForms.Properties.Resources.func_dados_71x62;
+            lblDados_Menu.Visible = false;
+        }
+
+        #endregion
+
+        private void btnDadosProfissionais_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Program.ultimaTela = 17;
+            telaFuncionarioDadosProfissionais telaafuncprofissionais = telaFuncionarioDadosProfissionais.getInstancia();
+            telaafuncprofissionais.Show();
+        }
+
+        private void btnDadosProfissionais_MouseEnter(object sender, EventArgs e)
+        {
+            this.btnDadosProfissionais.BackgroundImage = global::GuiWindowsForms.Properties.Resources.func_profissional_64x71_disabled_h;
+            lblProfissionais_menu.Visible = true;
+        }
+
+        private void btnDadosProfissionais_MouseLeave(object sender, EventArgs e)
+        {
+            this.btnDadosProfissionais.BackgroundImage = global::GuiWindowsForms.Properties.Resources.func_profissional_64x71_disabled;
+            lblProfissionais_menu.Visible = false;
+        }
+    }
+}
